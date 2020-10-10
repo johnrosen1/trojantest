75c82a306d04fad1aa0901fdb293eea2cd4ba5dc
johnrosen <john@johnrosen.xyz>
2020-10-10 19:32:41 +0800

    Update trojantest.sh

--- trojantest.sh
+++ trojantest.sh
@@ -18,8 +18,8 @@ case $key in
     shift # past argument
     shift # past value
     ;;
-    -l|--lib)
-    LIBPATH="$2"
+    -f|--file)
+    FILEPATH="$2"
     shift # past argument
     shift # past value
     ;;
@@ -125,9 +125,9 @@ local_port=$(( $RANDOM % 10000 + 50000 ))
 	cat > "${Domain}.json" << EOF
 {
 	"run_type": "client",
-	"local_addr": "127.0.0.1",
+	"local_addr": "0.0.0.0",
 	"local_port": ${local_port},
-	"remote_addr": "${Domain}",
+	"remote_addr": "${remoteip}",
 	"remote_port": 443,
 	"password": [
 		"${Password}"
@@ -139,7 +139,7 @@ local_port=$(( $RANDOM % 10000 + 50000 ))
 		"cert": "",
 		"cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA:AES128-SHA:AES256-SHA:DES-CBC3-SHA",
 		"cipher_tls13": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
-		"sni": "",
+		"sni": "${Domain}",
 		"alpn": [
 			"h2",
 			"http/1.1"
@@ -219,4 +219,8 @@ else
 	echo "---Telegram测试通过---"
 fi
 
-echo "-----测试完成-----"
+systemctl stop trojan@${Domain}
+
+echo "-----测试完成,结束进程-----"
+
+exit 0;
